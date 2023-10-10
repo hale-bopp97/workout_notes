5e74940f27aae1bbc74a3b31c98cb4311bcce9c5
hale-bopp97 <mattastik@gmail.com>
2023-10-10 08:59:38 -0700

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes) //vid 3 @ 5:15
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
